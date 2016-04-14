d957814caf02efa84f4b95c5dbea684a0554e0e7
Clarissa Stark <clarissa.stark@gmail.com>
2016-04-14 15:10:15 -0400

    made cnd links https in layout-main

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {};
 
 config.db = {};
 // the URL shortening host - shortened URLs will be this + base58 ID
-config.webhost = "http://localhost:3001";
+config.webhost = "https://localhost:3001/";
 
 // your MongoDB host and database name
 config.db.host = "localhost";
--- public/js/shorten.js
+++ public/js/shorten.js
@@ -7,10 +7,11 @@ $(".btn-shorten").on("click", function(){
     data: {url: $("#url-field").val()},
     success: function(data){
         // display the shortened URL to the user that is returned by the server
-        var resultHTML = "<a class='result' href='" + data.shortUrl + "'>" + data.shortUrl + '</a>';
+        var resultHTML = '<a class="result" href="' + data.shortUrl + '">'
+            + data.shortUrl + '</a>';
         $("#link").html(resultHTML);
         $("#link").hide().fadeIn("slow");
     }
   });
 
-})
+});
